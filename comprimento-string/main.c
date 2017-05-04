22ac84772585f1dddae916f29847a9409594226f
Bruno Andrade <brunoefa@gmail.com>
2017-05-04 19:37:13 -0300

    Comando strlen e lib string.h

---
+++ comprimento-string/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+FUAQ Leia 5 nomes e escreva na tela todos os nomes que possuam mais de 15 caracteres.
+*/
+
+int main() {
+
+    char nome[25];
+
+    for(int i = 0; i < 5; i++) {
+        printf("Informe o nome %d: ", i + 1);
+        gets(nome);
+        if (strlen(nome) > 15){
+            printf("Nome selecionado: %s\n", nome);
+        }
+    }
+    return 0;
+}
